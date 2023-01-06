d6e75ff860761b7b819d04546b1965210e4a91ec
Nathaniel <ndm0113@gmail.com>
2023-01-05 22:17:59 -0500

    Massive refactoring of runner - Better separation of concerns - Splitting out methods for unit testing - Revert bad fix in #17 - Improve fix for #14

--- daemon/__main__.py
+++ daemon/__main__.py
@@ -4,6 +4,8 @@ from time import time
 from threading import Timer
 from queue import Queue
 
+from pylast import LastFMNetwork
+
 from core import Runner, Database
 
 logger = logging.getLogger("daemon")
@@ -18,14 +20,26 @@ def run():
 
     logger.info("Connecting to database")
     db = Database("users.sqlite", getenv('SECRET_KEY'))
-    runner = Runner(db)
+    runner = Runner()
 
     logger.info("Started run, pulling users")
     users = db.get_users_for_run(int(time()))
     for user in users:
-        runner.run_user(user)
+        logger.info("Running update for user %s (%s)", user.name, user.uuid)
+        last_run, last_id = runner.run(get_pylast(user), Runner.get_ytm(user.cookie), user.last_run, user.last_id,
+                                       user.split_title)
+        if last_run > user.last_run:
+            db.update_last_run(user.uuid, last_run)
+        if last_id != user.last_id:
+            db.update_last_id(user.uuid, last_id)
+
     logger.info("Ran stats for %i users", len(users))
     db.con.close()
+
+
+def get_pylast(user):
+    # Work around a bug in pylast: https://github.com/pylast/pylast/issues/300
+    return LastFMNetwork(getenv('LASTFM_API'), getenv('LASTFM_SECRET'), user.token, user.name)
 
 
 run()
